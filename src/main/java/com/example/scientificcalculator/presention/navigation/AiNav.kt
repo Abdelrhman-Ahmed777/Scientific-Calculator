fc4f050e35fdd247d2546377a7385989a6fe0f39
Ab_aboelhassan <alainx796@gmail.com>
2024-12-09 01:14:54 +0200

    finishing the history feature

--- src/main/java/com/example/scientificcalculator/presention/navigation/AiNav.kt
+++
@@ -1,8 +0,0 @@
-package com.example.scientificcalculator.presention.navigation
-
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-
-fun NavGraphBuilder.aiAssistant(navController: NavController) {
-
-}
